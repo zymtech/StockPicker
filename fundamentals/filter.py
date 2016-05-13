14a52df218130d5f71710c50620d60aa2b5fbaf9
ymzhang <ym_zhang@foxmail.com>
2016-05-13 12:26:50 +0800

    blah,blah,blah ,nothing useful, commit to synchronize project skeleton

---
+++ fundamentals/filter.py
@@ -0,0 +1,11 @@
+import tushare
+
+class fundamentalfilter(object):
+    def __init__(self):
+        pass
+
+    def roe(self,threshold):
+        pass
+
+if __name__=='__main__':
+    pass
---
+++ overreaction/overreaction.py
@@ -0,0 +1,6 @@
+# encoding = "utf-8"
+
+
+class overreaction(object):
+    """implement of overreaction model in stock market"""
+    pass
